55bc5c5de2bae05bd815712cf97216b44de8a8de
Unknown <jeongsup.byun@gmail.com>
2019-09-07 12:40:56 +1000

    updated to make import statement more succinct

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# pkg_python
-Collection of custom python packages
+# Sample .bash_profile:
+
+alias python='python3'
+alias pip='pip3'
+export PYTHONPATH="/Users/jbyun/Library/Mobile Documents/iCloud~com~omz-software~Pythonista3/Documents/MyLibrary/Python3/pkg_python:/Users/jbyun/Library/Mobile Documents/iCloud~com~omz-software~Pythonista3/Documents/MyLibrary/Python3:$PYTHONPATH"
+export JUPYTER_PATH=$PYTHONPATH
--- strict_dataframe/__init__.py
+++ strict_dataframe/__init__.py
@@ -1,5 +1,44 @@
-__all__ = ['generic_types','pymonad_types','dataframe_types']
+__all__ = [
+	# generic_types 
+	'T',
 
-# from .generic_types import *
-# from .pymonad_types import *
+	# # typing
+	# 'TypeVar',
+	# 'Type',
+	# 'Dict',
+	# 'Set',
+	# 'Union',
+	# 'List',
+	# 'Generic',
+	# 'Any',
+	# 'Tuple',
+	# 'Callable',
+
+	# pymonad_types
+	'Container',
+	'Functor',
+	'Applicative',
+	'Monad',
+	'Reader',
+	'curry',
+	'Monoid',
+	'Maybe',
+	'Just',
+	'Nothing',
+	'First',
+	'Last',
+
+	# dataframe_types
+	'DataFrame',
+	'BasicType',
+	'TypeDict',
+	'StringList',
+	'StringSet',
+	'StringTypeTupleList',
+	'Printable',
+	'CumulativeMonoidSet',
+	'StrictDataFrame',
+	'HasStrictDataframe'
+]
+
 from .dataframe_types import *
